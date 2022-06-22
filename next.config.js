364830940cf395134079a1b0adafb0f38bbae276
floatflower <floatflower1029@gmail.com>
2022-06-22 19:27:49 +0000

    i18n locale

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const nextTranslate = require('next-translate')
 // /** @type {import('next').NextConfig} */
 module.exports = nextTranslate({
-  reactStrictMode: true,
-  i18n: {
-    locales: ['zh-Hant', 'zh-Hans', 'en', 'jp'],
-    defaultLocale: 'zh-Hant'
-  }
+  reactStrictMode: true
 })
